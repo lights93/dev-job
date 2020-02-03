bd2eb2a588bb49c271d7c4489dd7ba43138f6696
minho <lights8615@gmail.com>
2020-02-03 19:02:09 +0900

    크롤 전체 가저오기 #11

--- src/main/java/com/mino/devjob/service/CrawlKakaoService.java
+++ src/main/java/com/mino/devjob/service/CrawlKakaoService.java
@@ -1,6 +1,8 @@
 package com.mino.devjob.service;
 
+import java.io.IOException;
 import java.time.LocalDate;
+import java.util.Objects;
 import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -17,10 +19,12 @@ import com.mino.devjob.repository.RecruitRepository;
 import com.mino.devjob.type.CompanyType;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 
 @Service("KAKAO")
 @RequiredArgsConstructor
+@Slf4j
 public class CrawlKakaoService implements CrawlService {
 	private static final String KAKAO_RECRUIT_URL = "https://careers.kakao.com";
 	private static final Pattern NUMBER_PART_PATTERN = Pattern.compile("\\d+");
@@ -33,65 +37,94 @@ public class CrawlKakaoService implements CrawlService {
 			.followRedirects(false)
 			.method(Connection.Method.GET)
 			.data("part", "TECHNOLOGY")
-			.data("page", "8")
+			.data("page", "1")
 			.execute()
 			.parse();
 
-		final Elements titles = document.select(".txt_tit");
-		final Elements companyTypes = document.select(".item_board .field_front");
-		final Elements links = document.select(".link_notice");
+		String trim = document.select(".link_job.link_job1").text().trim();
 
-		final Elements details = document.select(".txt_period");
-		final Elements tagElements = document.select(".list_tag.wrap_tag");
+		Matcher countMatcher = NUMBER_PART_PATTERN.matcher(trim);
+		int pages = 0;
 
-		int cnt = titles.size();
+		if (countMatcher.find()) {
+			pages = (Integer.parseInt(countMatcher.group(0)) + 10 - 1) / 10;
+		}
 
-		return Flux.range(0, cnt)
+		return Flux.range(1, pages)
 			.map(i -> {
-				Element detail = details.get(i);
-				Elements detailTexts = detail.select("span span:not(.txt_bar)");
-				String jobType = "";
-				String term = "";
-				if (detailTexts.size() > 0) {
-					jobType = detailTexts.get(1).text().trim();
-					term = detailTexts.get(2).text().trim();
+				Document doc = null;
+				try {
+					doc = Jsoup.connect(KAKAO_RECRUIT_URL + "/jobs")
+						.followRedirects(false)
+						.method(Connection.Method.GET)
+						.data("part", "TECHNOLOGY")
+						.data("page", Integer.toString(i))
+						.execute()
+						.parse();
+				} catch (IOException e) {
+					log.error("kakao page parse error , page={}", i, e);
 				}
 
-				Matcher matcher = NUMBER_PART_PATTERN.matcher(term);
-				LocalDate end = Flux.fromStream(matcher.results())
-					.map(MatchResult::group)
-					.collectList()
-					.filter(list -> list.size() > 2)
-					.map(list -> LocalDate.of(Integer.parseInt(list.get(0)), Integer.parseInt(list.get(1)),
-						Integer.parseInt(list.get(2))))
-					.blockOptional()
-					.orElse(LocalDate.of(2999, 12, 31));
-
-				Element tagEl = tagElements.get(i);
-				Elements tagTexts = tagEl.select("a");
-
-				String tags = String.join(", ", tagTexts.eachText());
-
-				String link = KAKAO_RECRUIT_URL + links.get(i).attr("href").trim();
-
-				int startIdx = link.indexOf("/jobs/P-") + 8;
-				int endIdx = link.indexOf("?part");
-
-				int index = Integer.parseInt(link.substring(startIdx, endIdx));
-
-				return Recruit.builder()
-					.index(index)
-					.company(CompanyType.KAKAO.name())
-					.title(titles.get(i).text())
-					.companyType(companyTypes.get(i).text())
-					.link(link)
-					.jobType(jobType)
-					.term(end)
-					.tags(tags)
-					.build();
+				return doc;
 			})
-			.filterWhen(r -> recruitRepository.existsByIndexAndCompany(r.getIndex(), r.getCompany())
-				.map(b -> !b))
-			.flatMap(recruitRepository::save);
+			.filter(Objects::nonNull)
+			.flatMap(doc -> {
+				final Elements titles = doc.select(".txt_tit");
+				final Elements companyTypes = doc.select(".item_board .field_front");
+				final Elements links = doc.select(".link_notice");
+
+				final Elements details = doc.select(".txt_period");
+				final Elements tagElements = doc.select(".list_tag.wrap_tag");
+
+				int cnt = titles.size();
+
+				return Flux.range(0, cnt)
+					.map(i -> {
+						Element detail = details.get(i);
+						Elements detailTexts = detail.select("span span:not(.txt_bar)");
+						String jobType = "";
+						String term = "";
+						if (detailTexts.size() > 0) {
+							jobType = detailTexts.get(1).text().trim();
+							term = detailTexts.get(2).text().trim();
+						}
+
+						Matcher matcher = NUMBER_PART_PATTERN.matcher(term);
+						LocalDate end = Flux.fromStream(matcher.results())
+							.map(MatchResult::group)
+							.collectList()
+							.filter(list -> list.size() > 2)
+							.map(list -> LocalDate.of(Integer.parseInt(list.get(0)), Integer.parseInt(list.get(1)),
+								Integer.parseInt(list.get(2))))
+							.blockOptional()
+							.orElse(LocalDate.of(2999, 12, 31));
+
+						Element tagEl = tagElements.get(i);
+						Elements tagTexts = tagEl.select("a");
+
+						String tags = String.join(", ", tagTexts.eachText());
+
+						String link = KAKAO_RECRUIT_URL + links.get(i).attr("href").trim();
+
+						int startIdx = link.indexOf("/jobs/P-") + 8;
+						int endIdx = link.indexOf("?part");
+
+						int index = Integer.parseInt(link.substring(startIdx, endIdx));
+
+						return Recruit.builder()
+							.index(index)
+							.company(CompanyType.KAKAO.name())
+							.title(titles.get(i).text())
+							.companyType(companyTypes.get(i).text())
+							.link(link)
+							.jobType(jobType)
+							.term(end)
+							.tags(tags)
+							.build();
+					})
+					.filterWhen(r -> recruitRepository.existsByIndexAndCompany(r.getIndex(), r.getCompany())
+						.map(b -> !b))
+					.flatMap(recruitRepository::save);
+			});
 	}
 }
