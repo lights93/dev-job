ba59a3bf94789927b482fc5f7f7a54aa5c2bac7a
lights93 <lights8615@gmail.com>
2022-05-21 19:50:41 +0900

    chore: 사용하지 않는 코드 제거

--- src/main/java/com/mino/devjob/book/repository/BookRepository.java
+++ src/main/java/com/mino/devjob/book/repository/BookRepository.java
@@ -4,12 +4,10 @@ import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.mino.devjob.book.model.Book;
-import reactor.core.publisher.Flux;
+
 import reactor.core.publisher.Mono;
 
 @Repository
 public interface BookRepository extends ReactiveMongoRepository<Book, Long> {
-	Flux<Book> findAllByFavoriteIsLessThan(int favorite);
-
 	Mono<Boolean> existsByIdAndFavoriteIsNot(Long id, int favorite);
 }
--- src/main/java/com/mino/devjob/book/service/BookService.java
+++ src/main/java/com/mino/devjob/book/service/BookService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 
 import com.mino.devjob.book.model.Book;
 import com.mino.devjob.book.repository.BookRepository;
-import com.mino.devjob.recruit.model.Recruit;
+
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -31,10 +31,6 @@ public class BookService {
 	public Mono<Book> update(Book book) {
 		return bookRepository.save(book);
 	}
-
-//	public Flux<Book> getBooks(int favorite) {
-//		return bookRepository.findAllByFavoriteIsLessThan(favorite);
-//	}
 
 	public Mono<Boolean> notExistsByIndexAndCompanyAndFavoriteIsNot(Book book, int favorite) {
 		return bookRepository.existsByIdAndFavoriteIsNot(book.getId(), favorite).map(b -> !b);
